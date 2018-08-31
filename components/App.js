ae444af0db8d350bf652e40ef543f0098b2f8e71
przemek <wosin7@gmail.com>
2018-08-31 21:47:54 +0200

    poprawki4

--- components/App.js
+++ components/App.js
@@ -60,17 +60,11 @@ App = React.createClass({
 
         return (
             <div style={styles}>
-                <h1>Gif searcher</h1>
-                <p>Find gif on <a href='http=://giphy'>giphy</a>. Press ENTER for start search more gifs.</p>
-                <Search 
-                    onSearch={this.handleSearch}
-                />
-                <Gif 
-                    loading={this.state.loading}
-                    url={this.state.gif.url}
-                    sourceUrl={this.state.gif.sourceUrl}
-                />
-            </div>    
+                <h1>Wyszukiwarka GIFow!</h1>
+                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <Search />
+            <Gif />
+          </div>
         )
     }
 })
