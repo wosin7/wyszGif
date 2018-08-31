a7eaa0fa8af272068e2d8bdd62cd56b7e7174418
przemek <wosin7@gmail.com>
2018-08-31 21:37:03 +0200

    poprawki2

--- components/Search.js
+++ components/Search.js
@@ -1,34 +1,41 @@
 Search = React.createClass({
-  getInitialState() {
-    return {
-      searchingText: ''
-    };
+  getInitialState: function() {
+      return {
+          searchingText: ''
+      };
   },
-  handleChange: function(event) {
-    var searchingText = event.target.value;
-    this.setState({searchingText: searchingText});
 
-    if (searchingText.length > 2) {
-      this.props.onSearch(searchingText);
-    }
+  handleChange: function(event) {
+      var searchingText = event.target.value;
+      this.setState({
+          searchingText: searchingText
+      });
+
+      if (searchingText.length > 2) {
+          this.props.onSearch(searchingText);
+      }
   },
 
   handleKeyUp: function(event) {
-    if (event.keyCode === 13) {
-      this.props.onSearch(this.state.searchingText);
-    }
+      if(event.keyCode ===13) {
+          this.props.onSearch(this.state.searchingText);
+      }
   },
+  
+  render: function() {
+      var styles = {
+          fontSize: '1.5em',
+          width: '90%',
+          maxWidth: '350px'
+      };
 
-render: function() {
-    var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
-
-    return <input
-             type="text"
-             onChange={this.handleChange}
-             onKeyUp={this.handleKeyUp}
-             placeholder="Tutaj wpisz wyszukiwaną frazę"
-             style={styles}
-             value={this.state.searchTerm}
-            />
-  }
+      return <input 
+               type='text'
+               onChange={this.handleChange}
+               onKeyUp={this.handleKeyUp}
+               placeholder='Please, here enter a phrase'
+               style={styles}
+               value={this.state.searchingText}
+              />
+  } 
 });
